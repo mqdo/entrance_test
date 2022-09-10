c94349a4dbaf3d0afe420f5a7ef66f0ed345fe4c
Minh Quan Do <minhquan98vn@gmail.com>
2022-09-10 19:57:24 +0700

    Add more solutions to problems

--- Problem Solving/allLongestStrings.js
+++ Problem Solving/allLongestStrings.js
@@ -1,7 +1,9 @@
 // Chương trình nhập vào một mảng các chuỗi
 // trả về mảng mới với các chuỗi có độ dài lớn nhất
 
-function allLongestStrings(inputArray) {
+// cách 1: dùng function và vòng lặp for
+
+function allLongestStrings1(inputArray) {
   const outputArray = [];
   let longest = 0;
   for (i=0; i < inputArray.length; i++) {
@@ -11,4 +13,22 @@ function allLongestStrings(inputArray) {
     }
   }
   return(outputArray);
+};
+
+// cách 2: dùng arrow function và hai vòng lặp forEach lần lượt
+
+const allLongestStrings2 = inputArray => {
+  const outputArray = [];
+  let longest = 0;
+  inputArray.forEach(element => {
+    if (element.length > longest) {
+      longest = element.length;
+    }
+  });
+  inputArray.forEach(element => {
+    if (element.length === longest) {
+      outputArray.push(element);
+    }
+  });
+  return outputArray;
 }
--- Problem Solving/alternatingSums.js
+++ Problem Solving/alternatingSums.js
@@ -1,17 +1,38 @@
 // Chương trình nhập mảng chứa cân nặng của mọi người
 // Trả về mảng chứa tổng cân nặng của 2 team
 
-function alternatingSums(inputArray) {
+// cách 1: dùng function và vòng lặp forEach
+
+function alternatingSums1(inputArray) {
   const outputArray = [];
   let weight1 = 0;
   let weight2 = 0;
-  for (i = 0; i < inputArray.length; i++) {
-    if (i === 0 || i%2 === 0) {
-      weight1 += new Number(inputArray[i]);
+  inputArray.forEach (element => {
+    if (
+      inputArray.indexOf(element) === 0 ||
+      inputArray.indexOf(element) % 2 === 0
+    ) {
+      weight1 += element;
     } else {
-      weight2 += new Number(inputArray[i]);
+      weight2 += element;
     }
+  });
+  outputArray.push(weight1, weight2);
+  return outputArray;
+};
+
+// cách 2: dùng arrow function và vòng lặp while với i nhảy bước
+
+const alternatingSums2 = inputArray => {
+  const outputArray = [];
+  let weight1 = 0;
+  let weight2 = 0;
+  let i = 0;
+  while (i + 1 <= inputArray.length) {
+    weight1 += inputArray[i];
+    inputArray[i+1] && (weight2 += inputArray[i+1]);
+    i = i + 2;
   }
   outputArray.push(weight1, weight2);
   return outputArray;
-}
+};
